d28068f9ae857cba2f2cba77d82b861fcf58976d
Artemon Chik <zagrousaspear@gmail.com>
2023-03-06 16:37:59 +0100

    completed hw-07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const galleryContainer = document.querySelector(".gallery");
+function createGalleryMarkup(object) {
+  return object
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+    </a>`;
+    })
+    .join("");
+}
+const allImages = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("afterbegin", allImages);
 console.log(galleryItems);
+const image = document.querySelector(".gallery__image");
+const title = image.title;
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionPosition: "bottom",
+  captionType: "attr",
+  captionsData: "title",
+  captionDelay: 250,
+});
